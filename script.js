f5326a31d8108cf4a16113cff50bb1b86d313987
thororen <78185467+thororen1234@users.noreply.github.com>
2024-04-03 21:41:22 -0400

    brrr

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function setDiscordAvatar(element, discordId, defaultAvatar) {
     element.src = `https://cdn.discordapp.com/avatars/${discordId}/${defaultAvatar}`;
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Check and set Discord avatars for each staff member
     const avatars = document.querySelectorAll('.avatar');
     avatars.forEach(avatar => {
-        avatar.onerror = function() {
+        avatar.onerror = function () {
             // If the avatar URL fails, load the specified Discord avatar
             const discordId = this.getAttribute('data-discord-id');
             const defaultAvatar = this.getAttribute('data-default-avatar');
